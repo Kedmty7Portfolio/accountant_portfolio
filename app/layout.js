64378ef4c67360f98b1935ff3f7a988d5491e474
mahmoud elbalhi <mahmoud01234elbalhi@gmail.com>
2025-11-20 13:56:27 +0200

    up

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ const changa = Changa({
 export const metadata = {
   title: "Personal Portfolio",
   description: "Personal Client Portfolio",
-  icons: [{ rel: "icon", url: "icon.svg", sizes: "180x180" }],
+  icons: [{ rel: "icon", url: "icon.ico" }],
 };
 
 export default function RootLayout({ children }) {
